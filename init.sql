195f7fd6a3aac7da6a93d5939099bcc7e817595b
Artem Harutyunyan <artem@mesosphere.io>
2016-06-07 18:05:12 +0100

    Modified DB schema to store reviewboard data.

--- init.sql
+++ init.sql
@@ -1,19 +1,36 @@
 CREATE TABLE IF NOT EXISTS "review_requests" (
-       "id"               INTEGER UNIQUE NOT NULL,
-       "time_added"       INTEGER NOT NULL,
-       "last_updated"     INTEGER NOT NULL,
-       "issue_open_count" INTEGER NOT NULL,
-       "summary"          TEXT    NOT NULL,
-       "ship_it_count"    INTEGER NOT NULL,
-       "status"           TEXT    NOT NULL,
-       "submitter"        INTEGER NOT NULL,
-       "bugs_closed"      TEXT
+       "id"                INTEGER PRIMARY KEY AUTOINCREMENT,
+       "review_request_id" INTEGER UNIQUE NOT NULL,
+       "time_added"        INTEGER NOT NULL,
+       "last_updated"      VARCHAR NOT NULL,
+       "issue_open_count"  INTEGER NOT NULL,
+       "summary"           TEXT    NOT NULL,
+       "ship_it_count"     INTEGER NOT NULL,
+       "status"            VARCHAR NOT NULL,
+       "submitter"         VARCHAR NOT NULL,
+       "bugs_closed"       VARCHAR
 );
 
-CREATE TABLE IF NOT EXISTS "reviews" (
-       "id"                INTEGER UNIQUE NOT NULL,
-       "reviewer_id"       INTEGER,
-       "ship_it"           BOOLEAN NOT NULL,
-       "timestamp"         INTEGER NOT NULL,
-       "review_request_id" INTEGER NOT NULL
+
+CREATE TABLE IF NOT EXISTS "review_instances" (
+       "id"                 INTEGER PRIMARY KEY AUTOINCREMENT,
+       "review_instance_id" INTEGER UNIQUE NOT NULL,
+       "ship_it"            BOOLEAN NOT NULL,
+       "timestamp"          INTEGER NOT NULL,
+       "reviewer"           INTEGER,
+       "review_request_id"  INTEGER NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS "comments" (
+       "id"                       INTEGER PRIMARY KEY AUTOINCREMENT,
+       "review_request_id"        INTEGER NOT NULL,
+       "review_instance_id"       INTEGER NOT NULL,
+       "style"                    BOOLEAN,
+       "text"                     VARCHAR,
+       "timestamp"                VARCHAR,
+       "issue_opened"             BOOLEAN,
+       "comment_id"               INTEGER NOT NULL,
+       "reviewer"                 VARCHAR NOT NULL,
+       "review_request_submitter" VARCHAR NOT NULL
 );
